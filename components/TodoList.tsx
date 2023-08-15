439d4e28af67b7f1e600015598dccbd51e903dc2
Pasha <pashaie@gmail.com>
2023-08-15 18:09:33 +0330

    todo-list

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,13 +1,27 @@
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
 import React, { Component } from "react";
 
 export default class TodoList extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.addTaskContainer}>
         <Text>New Task</Text>
-        <TextInput></TextInput>
+        <TextInput style={styles.addTaskInput}></TextInput>
+        <Button title="Add"></Button>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  addTaskContainer: {
+    height: 50,
+    flexDirection: "row-reverse",
+    alignItems: "center",
+  },
+  addTaskInput: {
+    width: 100,
+    borderWidth: 1,
+    margin: 10,
+  },
+});
